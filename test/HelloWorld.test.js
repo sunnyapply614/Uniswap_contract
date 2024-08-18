3bca79a6e333ef915f95c7fcabd8752f708e8e2b
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-08-18 08:29:50 +0300

    Update HelloWorld.test.js

--- test/HelloWorld.test.js
+++ test/HelloWorld.test.js
@@ -3,8 +3,11 @@ const { ethers } = require("hardhat");
 
 describe("HelloWorld", function(){
     it("Should print a hello world greeting", async function () {
-        /* Deploy the helloWorld contract */
         
+        /* Deploy the helloWorld contract */
+        const helloWorldFactory = await ethers.getContractFactory("HelloWorld");
+        const helloWorld = await helloWorldFactory.deploy("World!");
+        await helloWorld.deployed();
 
         const greeting = await helloWorld.greet(); 
         expect(greeting).is.equal("Hello World!")
